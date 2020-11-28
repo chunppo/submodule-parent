03ffdfeb4efdd5fbaf87b992a38d3b5e887c587c
chunppo <chunsehyun@nate.com>
2020-11-29 01:37:36 +0900

    test

--- src/main/java/com/chunppo/MainController.java
+++ src/main/java/com/chunppo/MainController.java
@@ -6,6 +6,7 @@ import com.module.sub.demo.util.CBean;
 public class MainController {
     public static void main(String[] args) {
         System.out.println("TEST");
+        System.out.println("A");
         CBean cBean = new CBean();
     }
 }
