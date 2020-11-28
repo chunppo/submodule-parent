12229c26868e113cfd90a2edce5cddc068ebeec0
chunppo <chunsehyun@nate.com>
2020-11-28 21:27:31 +0900

    test

---
+++ src/main/java/com/chunppo/MainController.java
@@ -0,0 +1,11 @@
+package com.chunppo;
+
+
+import com.module.sub.demo.util.CBean;
+
+public class MainController {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+        CBean cBean = new CBean();
+    }
+}
